c67b9edf3fab8bb9e16e4c3fb103d7b366c11b70
Caike Bittin <caike.bittin@tbfg.com.br>
2022-08-22 09:37:05 -0300

    Adicionar arquivos de projeto.

---
+++ PDV_WPF_LOGIN/Tela_Inicial.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PDV_WPF_LOGIN
+{
+    /// <summary>
+    /// Lógica interna para Tela_Inicial.xaml
+    /// </summary>
+    public partial class Tela_Inicial : Window
+    {
+        public Tela_Inicial()
+        {
+            InitializeComponent();
+        }
+
+        private void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+    }
+}
